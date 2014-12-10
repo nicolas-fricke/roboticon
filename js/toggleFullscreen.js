0d1a542fb770e251646205d8aa47f4db66be49d6
Nicolas Fricke <mail@nicolasfricke.de>
2014-12-10 12:03:31 +0100

    Add toggle fullscreen to robot.html, press T to toggle

---
+++ js/toggleFullscreen.js
@@ -0,0 +1,73 @@
+var fullscreen = 'nothing';
+var initialHtmlBgColor;
+var initialBodyMargin;
+var initialVideoHeight;
+var initialVideoWidth;
+
+prepareToggle = function() {
+  $('#container').hide();
+  $('#container-fullscreen-video').hide();
+  $('#container-fullscreen-roboticon').hide();
+}
+prepareFullscreen = function() {
+  initialHtmlBgColor = initialHtmlBgColor || $('html').css('background-color');
+  $('html').css('background-color', 'black');
+  initialBodyMargin = initialBodyMargin || $('body').css('margin');
+  $('body').css('margin', 0);
+  initialVideoHeight = initialVideoHeight || $('#remoteVideo').css('height');
+  initialVideoWidth = initialVideoWidth || $('#remoteVideo').css('width');
+  prepareToggle();
+}
+prepareNormal = function() {
+  $('html').css('background-color', initialHtmlBgColor);
+  $('#remoteVideo').css('height', initialVideoHeight);
+  $('#remoteVideo').css('width', initialVideoWidth);
+  $('body').css('margin', initialBodyMargin);
+  prepareToggle();
+}
+
+setFullscreenNone = function() {
+  prepareNormal();
+  $('#remote-video-container').append($('#remoteVideo'));
+
+  $('#container').show();
+  fullscreen = 'nothing';
+}
+setVideoFullscreen = function() {
+  prepareFullscreen();
+  $('#container-fullscreen-video').append($('#remoteVideo')).show();
+  $('#remoteVideo').width($(window).width());
+  $('#remoteVideo').height($(window).height());
+  fullscreen = 'video';
+}
+setRoboticonFullscreen = function() {
+  prepareFullscreen();
+  $('#container-fullscreen-roboticon').show();
+  $('.roboticon').height($(window).height());
+  fullscreen = 'roboticon';
+}
+
+toggleFullscreen = function() {
+  switch (fullscreen) {
+    case 'nothing':
+      setVideoFullscreen();
+      break;
+    case 'video':
+      setRoboticonFullscreen();
+      break;
+    case 'roboticon':
+      setFullscreenNone();
+      break;
+  }
+  console.log('toggeling fullscreen to ' + fullscreen + '!');
+}
+
+keyHandler = function(e) {
+  var tag = e.target.tagName.toLowerCase();
+  if (e.which === 84 && tag != 'input' && tag != 'textarea')
+    toggleFullscreen();
+}
+
+$(document).ready(function() {
+  $('body').keyup(keyHandler);
+});
